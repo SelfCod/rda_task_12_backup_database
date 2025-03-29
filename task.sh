64aecbe355a0f877f7dd4fe519585c0bda5a41fb
SelfCod <ura.pikh@gmail.com>
2025-03-29 03:51:59 +0200

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+set -e
 
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve
 mysqldump --no-create-info -u "$DB_USER" -p"$DB_PASSWORD" ShopDB | mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment
